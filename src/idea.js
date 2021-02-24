af6cb175b955b3ddea16626c51d42ea15ebb7953
Riley Martin <riley.willow@gmail.com>
2021-02-24 15:58:16 -0700

    Updated idea class

--- src/idea.js
+++ src/idea.js
@@ -1,12 +1,13 @@
 class Idea  {
-  constructor(title, body, star) {
+  constructor(title, body) {
     this.id = Date.now();
     this.title = title;
     this.body = body;
     this.star = false;
   }
   saveToStorage() {
-
+//saves to storage
+//an array and/or local storage?
   }
   deleteFromStorage() {
 
