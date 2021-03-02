fffd8c673d36f6f357f89a6aa4262adbe62481b5
Riley Martin <riley.willow@gmail.com>
2021-03-02 08:52:24 -0700

    Create addComment and formatComment functions

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 //****Global Variables ****
 var newIdea;
 var ideas = [];
-
-
-
-
-
 // **** querySelectors ****
 var saveButton = document.querySelector("#saveIdeaButton");
 var title = document.querySelector("#titleInput");
@@ -13,8 +8,6 @@ var body = document.querySelector("#bodyInput");
 var searchBar = document.querySelector("#searchInput");
 var ideaBoard = document.querySelector("#ideaBoard");
 var toggleStarredIdeasButton = document.querySelector("#showIdeasButton");
-
-
 //**** Event Listeners ****
 ideaBoard.addEventListener("click", deleteIdea);
 ideaBoard.addEventListener("click", favoriteIdea);
@@ -24,42 +17,40 @@ body.addEventListener("input", showSave);
 searchBar.addEventListener("input", filterIdeas);
 ideaBoard.addEventListener("click", commentIdea);
 ideaBoard.addEventListener("click", addComment);
-
 toggleStarredIdeasButton.addEventListener("click", toggleStarredIdeas);
 window.addEventListener("load", renderPage);
-
-
-
 //**** Functions ****
 function saveIdea(event) {
   event.preventDefault();
   if (!title.value || !body.value) {
     return
   }
-
   newIdea = new Idea(title.value, body.value, false, Date.now());
-  
   newIdea.saveToStorage(newIdea);
   ideas.push(newIdea);
   render(ideas);
   clearInputs();
   saveButton.classList.add('disable-save');
 }
-
 function renderPage() {
   var retrievedIdeas = JSON.parse(localStorage.getItem("ideas"));
-  
-  for (var i = 0; i < retrievedIdeas.length; i++) {
-    newIdea = new Idea(retrievedIdeas[i].title, retrievedIdeas[i].body, retrievedIdeas[i].star, retrievedIdeas[i].id);
-    ideas.push(newIdea);
+  if (retrievedIdeas) {
+    for (var i = 0; i < retrievedIdeas.length; i++) {
+      var currentIdea = retrievedIdeas[i];
+      var storedComments = [];
+      var currentIdeasComments = currentIdea.comments;
+      for (var k = 0; k < currentIdeasComments.length; k++) {
+        var comment = new Comment(currentIdeasComments[k].ideaId, currentIdeasComments[k].content)
+        storedComments.push(comment);
+      }
+      newIdea = new Idea(currentIdea.title, currentIdea.body, currentIdea.star, currentIdea.id, storedComments);
+      ideas.push(newIdea);
+    }
   }
-  
   render(ideas);
 }
-
 function render(arrayToRender) {
     var markup = "";
-
     for (var i = 0; i < arrayToRender.length; i++) {
       var imageSource = getImageSourceFromIdea(arrayToRender[i]);
         markup += `
@@ -71,23 +62,26 @@ function render(arrayToRender) {
           <div class="card-text">
             <h3>${arrayToRender[i].title}</h3>
             <p class="card-body">${arrayToRender[i].body}</p>
+             <p class="comments-section" id="commentsSection">
+              Comments:
+              <ul>
+                ${formatComments(arrayToRender[i].comments)}
+              </ul>
+            </p>
           </div>
           <div class="card-bottom-bar">
             <input type="image" class="comment-button" id="commentButton" alt="Add comment" src="./images/comment.svg">
             Comment
             <div class="hidden">
               <input class="comment-input inputs type="text" name="comment" value="">
-              <button class="add-comment-button" id="addCommentButton">Add Comment</button>
-              <p class="comments-section" id="commentsSection">${arrayToRender[i].comments}</p>
+              <button class="add-comment-button button" id="addCommentButton">Add Comment</button>
             </div>
           </div>
         </article>
         `;
     }
-
     ideaBoard.innerHTML = markup;
 }
-
 function getImageSourceFromIdea(idea) {
   if (!idea.star) {
     return "./images/star.svg"
@@ -95,64 +89,52 @@ function getImageSourceFromIdea(idea) {
     return "./images/star-active.svg"
   }
 }
-
 function clearInputs() {
   title.value = null;
   body.value = null;
 }
-
-
 function showSave () {
   if (title.value && body.value) {
     saveButton.classList.remove('disable-save');
   }
 }
-
 function favoriteIdea(event) {
   if (event.target.id === "favoriteButton") {
-    var ideaId = event.target.closest("article").id;
-
-    ideas[ideaId].updateIdea();
-    //ideas[ideaId].saveToStorage(ideas[ideaId]);
+    var ideaIndex = event.target.closest("article").id;
+    ideas[ideaIndex].updateIdea();
     render(ideas);
   }
 }
-
 function deleteIdea(event) {
   if (event.target.id === "deleteButton") {
-    var ideaId = event.target.closest("article").id;
-    
-    ideas[ideaId].deleteFromStorage();
-    ideas.splice(ideaId, 1); 
-    
+    var ideaIndex = event.target.closest("article").id;
+    ideas[ideaIndex].deleteFromStorage();
+    ideas.splice(ideaIndex, 1);
     render(ideas);
   }
 }
-
 function commentIdea(event) {
   if (event.target.id === "commentButton") {
     var commentInputAndButton = event.target.nextElementSibling;
     commentInputAndButton.classList.remove("hidden");
   }
 }
-
-function addComment() {
+function addComment(event) {
   if (event.target.id === "addCommentButton") {
-    var ideaId = event.target.closest("article").id;
-    var commentInput = event.target.previousElementSibling;
-    var newComment = new Comment(ideaId, commentInput.value);
-
-    for (var i = 0; i < ideas.length; i++) {
-      if (parseInt(ideaId) === ideas[i].id) {
-        ideas[i].comments += newComment.content;
-      }
-    }
-    localStorage.setItem("ideas", JSON.stringify(ideas));
+    var ideaIndex = event.target.closest("article").id;
+    var commentInput = event.target.previousElementSibling.value;
+    var newComment = new Comment(ideas[ideaIndex].id, commentInput);
+    ideas[ideaIndex].storeComment(newComment)
     render(ideas);
-    //comments not rendering on browser, but storing with correct instance of idea class
   }
 }
-
+function formatComments(commentArray) {
+  var commentMarkup = "";
+  for (var i = 0; i < commentArray.length; i++) {
+    commentMarkup += `<li>${commentArray[i].content}</li>`
+  }
+  return commentMarkup
+}
 function toggleStarredIdeas(event) {
   if (event.srcElement.innerText === "Show Starred Ideas") {
     toggleStarredIdeasButton.innerHTML = "Show All Ideas";
@@ -173,11 +155,9 @@ function toggleStarredIdeas(event) {
     render(ideas);
   }
 }
-
 function filterIdeas() {
   var searchQuery = searchBar.value.toLowerCase();
   var matchingIdeas = [];
-
   for (var i = 0; i < ideas.length; i++) {
     if (ideas[i].title.toLowerCase().includes(searchQuery) || ideas[i].body.toLowerCase().includes(searchQuery)) {
       matchingIdeas.push(ideas[i]);
